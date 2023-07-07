bbfee285d680ee3ad86e58c47906da43c4cfe7eb
crCoelhosm <cristophersaar@gmail.com>
2023-07-07 14:40:38 -0500

    update

--- postget.js
+++ postget.js
@@ -4,52 +4,23 @@ const app = express();
 
 app.use(express.json());
 
-
-// definição do nome do arquivo
 const filePath = 'names.json';
 
-
-// metodo GET para visualizar o arquivo names.json
 app.get('/get-name', (req, res) => {
-
-    // o err recebe erro, caso haja. o data recebe o conteudo do arquivo names.js(o filePath)
     fs.readFile(filePath, 'utf8', (err, data) => {
-
-        // caso haja erro:
         if (err) {
-
-            // resposta no console ao erro
             console.error('Erro ao ler o arquivo:', err);
-
-            //  resposta ao erro
             res.status(500).json({ error: 'Erro ao ler o arquivo' });
-        }
-
-        else {
+        } else {
             const names = JSON.parse(data);
-
-            // retorna o objeto que compoe o arquivo
             res.json(names);
         }
-
     });
-
 });
 
-// metodo POST  para enviar um objeto para o arquivo names.json
 app.post('/send-name', (req, res) => {
-
-    // atributos do corpo da mensagem
-    const { id, nome } = req.body;
-
-    // compõe o que vai ser usado para o POST
-    const newPost = { id, nome };
-
     fs.readFile(filePath, 'utf8', (err, data) => {
-
-        // o err recebe erro, caso haja. o data recebe o conteudo do arquivo names.js(o filePath)
         if (err) {
-            // resposta ao erro
             console.error('Erro ao ler o arquivo:', err);
             res.status(500).json({ error: 'Erro ao ler o arquivo' });
             return;
@@ -57,32 +28,58 @@ app.post('/send-name', (req, res) => {
 
         const names = JSON.parse(data);
 
-        // Verificar se já existe um objeto com o mesmo ID
-        const existingPost = names.find(post => post.id === id);
-        if (existingPost) {
-            res.status(400).json({ error: 'ID já existe' });
-            return;
-        }
+        // busca o maior ID existente
+        const maxId = names.reduce((max, name) => Math.max(max, name.id), 0);
 
-        // insere o POST no final do documento
+        const { nome } = req.body;
+        const newPost = {
+            id: maxId + 1, // incrementa o ID automaticamente
+            nome,
+        };
+
         names.push(newPost);
 
         fs.writeFile(filePath, JSON.stringify(names), (err) => {
-            // caso haja um erro
             if (err) {
                 console.error('Erro ao salvar o arquivo:', err);
-                // resposta negativa
                 res.status(500).json({ error: 'Erro ao salvar o arquivo' });
             } else {
-                // resposta positiva ao método
-                res.json({ message: 'Post criado com sucesso' });
+                res.json({ message: 'Post criado com sucesso', post: newPost });
             }
         });
     });
 });
 
-app.delete('/delete-name', (req, res) => {
-    const { id } = req.body;
+app.delete('/delete-name/:id', (req, res) => {
+    const { id } = req.params;
+  
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Erro ao ler o arquivo:', err);
+        res.status(500).json({ error: 'Erro ao ler o arquivo' });
+        return;
+      }
+  
+      let names = JSON.parse(data);
+  
+      // Filtra os objetos com IDs diferentes do ID fornecido
+      names = names.filter((name) => name.id !== parseInt(id));
+  
+      fs.writeFile(filePath, JSON.stringify(names), (err) => {
+        if (err) {
+          console.error('Erro ao salvar o arquivo:', err);
+          res.status(500).json({ error: 'Erro ao salvar o arquivo' });
+        } else {
+          res.json({ message: 'Objeto deletado com sucesso' });
+        }
+      });
+    });
+  });
+  
+
+app.put('/update-name/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome } = req.body;
 
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
@@ -93,27 +90,34 @@ app.delete('/delete-name', (req, res) => {
 
         let names = JSON.parse(data);
 
-        // Filtra os objetos com IDs diferentes do ID fornecido
-        names = names.filter((name) => name.id !== id);
+        // Procura o objeto com o ID fornecido
+        const nameToUpdate = names.find((name) => name.id === parseInt(id));
+
+        if (!nameToUpdate) {
+            res.status(404).json({ error: 'Nome não encontrado' });
+            return;
+        }
+
+        // Atualiza o nome
+        nameToUpdate.nome = nome;
 
         fs.writeFile(filePath, JSON.stringify(names), (err) => {
             if (err) {
                 console.error('Erro ao salvar o arquivo:', err);
                 res.status(500).json({ error: 'Erro ao salvar o arquivo' });
             } else {
-                res.json({ message: 'Objeto deletado com sucesso' });
+                res.json({ message: 'Nome atualizado com sucesso', name: nameToUpdate });
             }
         });
     });
 });
 
-// verifica se o arquivo existe
+
+
 if (!fs.existsSync(filePath)) {
-    // cria o arquivo vazio caso não exista
     fs.writeFileSync(filePath, '[]', 'utf8');
 }
 
-// escuta e avisa quando o servidor iniciar
 app.listen(3000, () => {
     console.log('Servidor ouvindo na porta 3000');
 });
