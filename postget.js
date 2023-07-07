00e3c512e4c9d653555320fc77d4a85bfd86fb63
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2023-07-07 14:58:14 -0500

    update + incremento + comentarios

--- postget.js
+++ postget.js
@@ -32,11 +32,19 @@ app.post('/send-name', (req, res) => {
         const maxId = names.reduce((max, name) => Math.max(max, name.id), 0);
 
         const { nome } = req.body;
+
+        //no lugar de pegar o id do body, procurar o maior id do documento
+        // usando a lib Math pra então definir que o proximo id adicionado será ele +1
+        // para que não precise que o usuario forneça o valor
+
         const newPost = {
             id: maxId + 1, // incrementa o ID automaticamente
             nome,
         };
 
+        // isso vai alterar o como o id está sendo passado para o arquivo, de string ("n"), para integer (n);
+
+        // e aqui envia o objeto alterado, do mesmo jeito que é realizado uma inserção no formato antigo
         names.push(newPost);
 
         fs.writeFile(filePath, JSON.stringify(names), (err) => {
@@ -90,7 +98,7 @@ app.put('/update-name/:id', (req, res) => {
 
         let names = JSON.parse(data);
 
-        // Procura o objeto com o ID fornecido
+        // Procura o objeto com o ID fornecido, e faz a conversão dele para inteiro para garantir que ele busque com o tipo correto guardado no documento names.json
         const nameToUpdate = names.find((name) => name.id === parseInt(id));
 
         if (!nameToUpdate) {
@@ -101,12 +109,13 @@ app.put('/update-name/:id', (req, res) => {
         // Atualiza o nome
         nameToUpdate.nome = nome;
 
+
         fs.writeFile(filePath, JSON.stringify(names), (err) => {
             if (err) {
                 console.error('Erro ao salvar o arquivo:', err);
                 res.status(500).json({ error: 'Erro ao salvar o arquivo' });
             } else {
-                res.json({ message: 'Nome atualizado com sucesso', name: nameToUpdate });
+                res.json({ message: 'Nome atualizado com sucesso', name: nameToUpdate });// escreve o nome atualizado no names.json, e fornece feedback para o usuario
             }
         });
     });
